ac2d7da91fa79af48fb910e769ac315e582ff023
Francis Fernandez <fosb.promo@gmail.com>
2020-02-13 23:52:57 -0800

    attempted the gradient descent problem'

--- machine-learning-ex1/ex1/gradientDescent.m
+++ machine-learning-ex1/ex1/gradientDescent.m
@@ -18,7 +18,10 @@ for iter = 1:num_iters
     %
 
 
-
+X = [ones(m, 1), data(:,1)]; % Add a column of ones to x
+theta = zeros(2, 1); % initialize fitting parameters
+iterations = 1500;
+alpha = 0.01;
 
 
 
