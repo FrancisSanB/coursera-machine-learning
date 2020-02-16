e23d51d7438f9582afe30815c0f264e3c5774d30
Francis Fernandez <fosb.promo@gmail.com>
2020-02-15 17:39:47 -0800

    completed and submited week 2 required work

--- machine-learning-ex1/ex1/ex1.m
+++ machine-learning-ex1/ex1/ex1.m
@@ -26,8 +26,7 @@
 %% Initialization
 clear ; close all; clc
 
-%% ==================== Part 1: Basic Function ====================
-% Complete warmUpExercise.m
+%% === Part 1: Basic Function ===
 fprintf('Running warmUpExercise ... \n');
 fprintf('5x5 Identity Matrix: \n');
 warmUpExercise()
@@ -36,21 +35,17 @@ fprintf('Program paused. Press enter to continue.\n');
 pause;
 
 
-%% ======================= Part 2: Plotting =======================
+%% === Part 2: Plotting ===
 fprintf('Plotting Data ...\n')
 data = load('ex1data1.txt');
 X = data(:, 1); y = data(:, 2);
 m = length(y); % number of training examples
 
-% Plot Data
-% Note: You have to complete the code in plotData.m
-plotData(X, y);
-
 fprintf('Program paused. Press enter to continue.\n');
 pause;
 
-%% =================== Part 3: Cost and Gradient descent ===================
 
+%% === Part 3: Cost and Gradient descent ===
 X = [ones(m, 1), data(:,1)]; % Add a column of ones to x
 theta = zeros(2, 1); % initialize fitting parameters
 
@@ -99,7 +94,8 @@ fprintf('For population = 70,000, we predict a profit of %f\n',...
 fprintf('Program paused. Press enter to continue.\n');
 pause;
 
-%% ============= Part 4: Visualizing J(theta_0, theta_1) =============
+
+%% === Part 4: Visualizing J(theta_0, theta_1) ===
 fprintf('Visualizing J(theta_0, theta_1) ...\n')
 
 % Grid over which we will calculate J
--- machine-learning-ex1/ex1/gradientDescent.m
+++ machine-learning-ex1/ex1/gradientDescent.m
@@ -3,32 +3,18 @@ function [theta, J_history] = gradientDescent(X, y, theta, alpha, num_iters)
 %   theta = GRADIENTDESCENT(X, y, theta, alpha, num_iters) updates theta by 
 %   taking num_iters gradient steps with learning rate alpha
 
-% Initialize some useful values
-m = length(y); % number of training examples
+m = length(y);
 J_history = zeros(num_iters, 1);
 
 for iter = 1:num_iters
-
-    % ====================== YOUR CODE HERE ======================
-    % Instructions: Perform a single gradient step on the parameter vector
-    %               theta. 
-    %
-    % Hint: While debugging, it can be useful to print out the values
-    %       of the cost function (computeCost) and gradient here.
-    %
-
-
-X = [ones(m, 1), data(:,1)]; % Add a column of ones to x
-theta = zeros(2, 1); % initialize fitting parameters
-iterations = 1500;
-alpha = 0.01;
-
-
-
-
-    % ============================================================
-
-    % Save the cost J in every iteration    
+    
+    k = 1:m;
+    t1 = sum((theta(1) + theta(2) .* X(k,2)) - y(k));
+    t2 = sum(((theta(1) + theta(2) .* X(k,2)) - y(k)) .* X(k,2));
+    
+    theta(1) = theta(1) - (alpha/m) * (t1);
+    theta(2) = theta(2) - (alpha/m) * (t2);
+    
     J_history(iter) = computeCost(X, y, theta);
 
 end
