4f887c98fa09865d8506e519402cf6a5e13d44b4
Francis Fernandez <fosb.promo@gmail.com>
2020-02-19 17:23:17 -0800

    finished week 4

--- Week 4/ex3/lrCostFunction.m
+++ Week 4/ex3/lrCostFunction.m
@@ -5,9 +5,14 @@ m = length(y); % number of training examples
 J = 0;
 grad = zeros(size(theta));
 
-h_theta = sigmoid(x)
-J = (1/m) .* ((-y).*log(h_theta) - (1 - y) .*log(1 - h_theta));
+h_theta = sigmoid(X * theta);
 
+J = (1/m) * (((-1 * y') * log(h_theta)) - ((1-y') * log(1-h_theta))) + (lambda/(2*m)) * (sum(theta(2:end) .^ 2));
+
+temp = theta;
+temp(1) = 0;
+
+grad = ((1 / m) * X' * (h_theta - y)) + (lambda / m) * temp;
 grad = grad(:);
 
 end
